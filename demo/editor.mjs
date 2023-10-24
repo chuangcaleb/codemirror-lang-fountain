06b8d38c33d0e426fad9ed8ffec6fde55960a785
Chuang Caleb <62867911+chuangcaleb@users.noreply.github.com>
2023-10-24 09:31:02 +0800

    fix: grammar, these vs this LOL

--- demo/editor.mjs
+++ demo/editor.mjs
@@ -7,7 +7,7 @@ EXT. BRICK'S PATIO - DAY
 
 A gorgeous day.  The sun is shining.  But BRICK BRADDOCK, retired police detective, is sitting quietly, contemplating -- something.
 
-= This are some editor's notes
+= These are some editor's notes
 
 The SCREEN DOOR slides open and DICK STEEL, his former partner and fellow retiree, emerges with two cold beers.
 
